68529c544ffe3d39b8a144f93d5411b6c93ac4a1
CIOSAI <jyinteractive.tw@gmail.com>
2023-09-26 00:52:47 +0800

    filtering

--- src/lib/ListView.svelte
+++ src/lib/ListView.svelte
@@ -1,18 +1,84 @@
 <script lang="ts">
-	import { wikifonts, type FontEntry } from '$lib/wikidata';
+	import { wikifonts } from '$lib/wikidata';
+	import { filterIndices } from '$lib';
 	import ListEntry from './ListEntry.svelte';
+
+	let filtered = $wikifonts;
+
+	$: filtered = $wikifonts.filter((row) => {
+		function similar(a: string, b: string) {
+			return a.toLowerCase().startsWith(b.toLocaleLowerCase());
+		}
+		if ($filterIndices.font !== '') {
+			return row.fontLabel && similar(row.fontLabel.value, $filterIndices.font);
+		}
+		if ($filterIndices.foundry !== '') {
+			return row.foundryLabel && similar(row.foundryLabel.value, $filterIndices.foundry);
+		}
+		if ($filterIndices.country !== '') {
+			return (
+				row.countryOfOperationLabel &&
+				similar(row.countryOfOperationLabel.value, $filterIndices.country)
+			);
+		}
+		return true;
+	});
 </script>
 
 <table class="text-lime-800 border-collapse">
 	<thead
-		><tr class="text-lime-800 text-lg text-center">
-			<td class="border-b-4 border-lime-800">name</td>
-			<td class="border-b-4 border-lime-800">made by</td>
-			<td class="border-b-4 border-lime-800">based in</td>
+		><tr class="text-lime-800">
+			<td class="border-b-4 border-lime-800 px-4">name</td>
+			<td class="border-b-4 border-lime-800 px-4">made by</td>
+			<td class="border-b-4 border-lime-800 px-4">based in</td>
 		</tr></thead
 	>
 	<tbody>
-		{#each $wikifonts as entry}
+		<tr class="text-lime-800 text-sm">
+			<td
+				><input
+					class="w-full my-4 bg-yellow-400 px-3 appearance-none border-b-2 border-lime-800 outline-none focus:bg-yellow-500 placeholder:text-yellow-600"
+					type="text"
+					placeholder="filter by name"
+					on:keyup={(e) => {
+						if (!e.target) return;
+
+						filterIndices.update((old) => {
+							return { font: e.target.value, foundry: old.foundry, country: old.country };
+						});
+					}}
+				/></td
+			>
+			<td
+				><input
+					class="w-full my-4 bg-yellow-400 px-3 appearance-none border-b-2 border-lime-800 outline-none focus:bg-yellow-500 placeholder:text-yellow-600"
+					type="text"
+					placeholder="filter by foundry"
+					on:keyup={(e) => {
+						if (!e.target) return;
+
+						filterIndices.update((old) => {
+							return { font: old.font, foundry: e.target.value, country: old.country };
+						});
+					}}
+				/></td
+			>
+			<td
+				><input
+					class="w-full my-4 bg-yellow-400 px-3 appearance-none border-b-2 border-lime-800 outline-none focus:bg-yellow-500 placeholder:text-yellow-600"
+					type="text"
+					placeholder="filter by country"
+					on:keyup={(e) => {
+						if (!e.target) return;
+
+						filterIndices.update((old) => {
+							return { font: old.font, foundry: old.foundry, country: e.target.value };
+						});
+					}}
+				/></td
+			>
+		</tr>
+		{#each filtered as entry}
 			<ListEntry {entry} />
 		{/each}
 	</tbody>
