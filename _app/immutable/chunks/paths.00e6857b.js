fadf7bdd71a9b9205e09647027a41c174fd7a300
CIOSAI <89374056+CIOSAI@users.noreply.github.com>
2023-09-26 13:28:59 +0000

    Deploying to gh-pages from @ CIOSAI/fonts-on-wiki@f55d9b6b9ab70f3c38a2a95ba111dd4059604886 🚀

---
+++ _app/immutable/chunks/paths.00e6857b.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_os4mmq)==null?void 0:s.base)??"/fonts-on-wiki";var t;const e=((t=globalThis.__sveltekit_os4mmq)==null?void 0:t.assets)??a;export{e as a,a as b};
--- _app/immutable/chunks/paths.06c3440a.js
+++
@@ -1,1 +0,0 @@
-var s;const i=((s=globalThis.__sveltekit_cn7i5b)==null?void 0:s.base)??"/CIOSAI.github.io/fonts-on-wiki";var t;const a=((t=globalThis.__sveltekit_cn7i5b)==null?void 0:t.assets)??i;export{a,i as b};
