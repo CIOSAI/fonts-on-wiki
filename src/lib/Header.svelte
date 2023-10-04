f4d387b3ddaefe32d90f7ce8b8ac290542a0ad30
CIOSAI <jyinteractive.tw@gmail.com>
2023-10-04 14:35:26 +0800

    added about page

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,6 +2,7 @@
 	import { base } from '$app/paths';
 </script>
 
-<div class="flex mx-4 my-2 text-lime-800">
+<div class="flex mx-4 my-2 gap-2 text-lime-800">
 	<a class="underline hover:italic" href={`${base}/`}>Home</a>
+	<a class="underline hover:italic" href={`${base}/about`}>About</a>
 </div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,19 @@
+<div class="text-lime-800">
+	<p>
+		All data presented on this app are from SPARQL API of <a
+			class="underline"
+			target="_blank"
+			rel="noopener noreferrer"
+			href="https://www.wikidata.org">Wikidata</a
+		>, there may be some inaccuracies. This is not suitable for real data research.
+	</p>
+	<p>
+		Made by <a
+			class="underline"
+			target="_blank"
+			rel="noopener noreferrer"
+			href="https://ciosai.github.io/">CIOSAI</a
+		>
+	</p>
+	<p>Made with Svelte + Tailwind</p>
+</div>
